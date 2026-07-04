ed8c759b2aceeace99738643e58171e0dfb7fd58
Carlos Eduardo da Silva Ribeiro <kaduesr@gmail.com>
2026-07-04 15:49:46 -0300

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,1 @@
+export default function App() { return React.createElement("div", {className:"app"}, React.createElement("h1", null, "weathernow"), React.createElement("p", null, "Veja o README para detalhes.")) }
